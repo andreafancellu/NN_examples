adfea20d3a282f71efd4cfecd433b644a96e75b4
andreafancellu <andreafancellu123@gmail.com>
2024-10-21 16:49:56 +0200

    first commit

---
+++ const.py
@@ -0,0 +1,10 @@
+### This File contains all the LaTeX code and images used in the Notebook ###
+
+# LaTeX
+autoencoder_formulas = r"""
+                        Encoder: \mathbf{h} = f(\mathbf{x}) \\
+                        Decoder: \mathbf{\hat{x}} = g(\mathbf{h}) 
+                        """
+
+# Images
+autoencoder_image = 'images/autoencoder.png'
